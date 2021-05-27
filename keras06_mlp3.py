9825121807421eab9f19cc05b87d854f52fdbb2d
rollingman1 <bloodtype0@khu.ac.kr>
2021-05-27 20:00:31 +0900

    accuracy 평가지표

--- keras06_mlp3.py
+++ keras06_mlp3.py
@@ -15,6 +15,9 @@ model.add(Dense(50))
 model.add(Dense(1))
 
 # 3. 컴파일, 훈련
+# model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+# model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+# model.compile(loss='mse', optimizer='adam', metrics=['accuracy']) # 회귀, 분류 중 분류에 쓰임. 예상 출력값과 100% 맞았을 때 true. 회귀에서는 0이 나올 수 밖에 없음.
 model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae']) # 여러 평가지표를 넣을 수 있음.
 model.fit(x, y, batch_size=1, epochs=10)
 
@@ -26,3 +29,8 @@ print("loss : ", loss)
 # 4/4 [==============================] - 0s 6ms/step - loss: 36.2823 - mse: 36.2823 - mae: 4.6923
 # 1/1 [==============================] - 0s 119ms/step - loss: 36.6343 - mse: 36.6343 - mae: 4.1494
 # loss :  [36.634307861328125, 36.634307861328125, 4.149420261383057]
+
+# Epoch 10/10
+# 4/4 [==============================] - 0s 3ms/step - loss: 37.6766 - accuracy: 0.0000e+00
+# 1/1 [==============================] - 0s 109ms/step - loss: 38.2353 - accuracy: 0.0000e+00
+# loss :  [38.23528289794922, 0.0]
