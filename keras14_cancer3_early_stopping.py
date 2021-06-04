1c1b0ecc538a73cea92417dd2ade26ac27a560fd
rollingman1 <bloodtype0@khu.ac.kr>
2021-06-04 20:12:54 +0900

    early stopping

--- keras14_cancer3_early_stopping.py
+++ keras14_cancer3_early_stopping.py
@@ -34,7 +34,7 @@ model.add(Dense(10, activation='relu', input_shape=(30,)))
 model.add(Dense(2, activation='softmax'))
 
 #3.
-es = EarlyStopping(monitor='val_acc', patience=25)
+es = EarlyStopping(monitor='val_loss', patience=10, mode='min')
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.1, callbacks=[es]) # early stopping
 
@@ -47,3 +47,19 @@ y_predict = model.predict(x_test)
 # print("input: ",x_test[:5])
 print("GT: ", y_test[:5])
 print("predict: ", y_predict[:5])
+
+# Epoch 45/100
+# 409/409 [==============================] - 0s 427us/step - loss: 0.3270 - acc: 0.9242 - val_loss: 0.0571 - val_acc: 0.9783
+# 4/4 [==============================] - 0s 634us/step - loss: 0.2973 - acc: 0.9123
+# loss :  0.29732832312583923
+# acc :  0.9122806787490845
+# GT:  [[0. 1.]
+#  [0. 1.]
+#  [0. 1.]
+#  [0. 1.]
+#  [0. 1.]]
+# predict:  [[6.0292851e-04 9.9939704e-01]
+#  [2.8536475e-04 9.9971467e-01]
+#  [1.6985410e-04 9.9983013e-01]
+#  [7.0037966e-04 9.9929965e-01]
+#  [3.2944898e-03 9.9670547e-01]]
